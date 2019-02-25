8ff243b44afb06ec1f5b15459b17b0992bb9fe21
Anton Trofimenko <a.trofimenko@macbook-pro-anton.local>
2019-02-25 17:31:17 +0300

    Initial Commit

---
+++ GitHubTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubTest
+//
+//  Created by Anton Trofimenko on 25/02/2019.
+//  Copyright © 2019 Anton Trofimenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
